3d563949d94df2ea580d9556fd83b75e673a0e25
Mariam <mariam.avetis@outlook.com>
2023-01-21 21:41:46 +0400

    Synchronize Protractor JS

--- spec.js
+++ spec.js
@@ -2,11 +2,8 @@ describe('angularjs homepage todo list', function() {
   it('Opens Angular JS website', function() {
     browser.get('https://angularjs.org');
 
-    browser.get('http://juliemr.github.io/protractor-demo/');
-
-    browser.sleep(5000);
-
-    console.log("test complete");
-
+    browser.get('http://juliemr.github.io/protractor-demo/').then(function() {
+      console.log("test complete")
+    });
   });
 });
