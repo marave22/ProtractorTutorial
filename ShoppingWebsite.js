669f7c4baf86ec9d6a6e34c9eb8c445afa66913f
Mariam <mariam.avetis@outlook.com>
2023-03-08 01:50:58 +0400

    Shopping website practice

---
+++ ShoppingWebsite.js
@@ -0,0 +1,7 @@
+describe('Practice with shopping website', function () {
+    it('Adding Product into the Cart', function () {
+        browser.get('https://rahulshettyacademy.com/seleniumPractise/#/');
+        browser.driver.manage().window().maximize();
+
+    })
+})
